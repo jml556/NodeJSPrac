ecb2f75055dee88bb958214fa975b41b7da941f4
Joe <jmleesjc2000@gmail.com>
2021-08-12 19:35:52 -0400

    html page

--- first-app.js
+++ first-app.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
 function rqListener(req, res) {
-
+    console.log(req)
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>')
+    res.write('<head><title>First practive node</title></head>')
+    res.write('<body><h1>Hello World!</h1></body>')
+    res.write('</html>')
+    res.end();
 }
 
-http.createServer();
+const server = http.createServer(rqListener);
+
+server.listen(3005)
