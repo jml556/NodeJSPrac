c49f4dd0e82ce47dffefdb95b33a26585636813e
Joe <jmleesjc2000@gmail.com>
2021-09-14 06:32:35 -0400

    updated

--- first-app.js
+++ first-app.js
@@ -5,10 +5,12 @@ const app = express()
 
 app.use((req, res, next) => {
     console.log('In the middlkeware')
+    next()
 })
 
-app.use()
+app.use((req, res, next) => {
+    console.log('In the next middlkeware')
+    res.send('<h1>Hello Worldd</h1>')
+})
 
-const server = http.createServer(app)
-
-server.listen(3000)
+app.listen(3000)
