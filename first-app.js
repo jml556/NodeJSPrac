0a58156326db8127f90442335f3afb77abca6068
MrSquash14 <76232474+MrSquash14@users.noreply.github.com>
2021-09-19 19:21:03 -0400

    update

--- first-app.js
+++ first-app.js
@@ -1,14 +1,22 @@
-const http = require('http')
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use((req, res, next) => {
+app.get('/favicon.ico', (req, res) => res.status(204));
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(('/add-product'), (req, res, next) => {
     console.log('In the middlkeware')
-    next()
+    res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>')
 })
 
-app.use((req, res, next) => {
+app.use('/product', (req, res, next) => {
+    console.log(req.body)
+    res.redirect('/')
+})
+
+app.use(('/'), (req, res, next) => {
     console.log('In the next middlkeware')
     res.send('<h1>Hello Worldd</h1>')
 })
