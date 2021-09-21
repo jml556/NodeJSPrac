8825950625f09085b57a67890a808268221f8a57
MrSquash14 <76232474+MrSquash14@users.noreply.github.com>
2021-09-21 19:20:41 -0400

    Updated express

--- first-app.js
+++ first-app.js
@@ -6,15 +6,7 @@ const app = express()
 app.get('/favicon.ico', (req, res) => res.status(204));
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.use(('/add-product'), (req, res, next) => {
-    console.log('In the middlkeware')
-    res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>')
-})
 
-app.use('/product', (req, res, next) => {
-    console.log(req.body)
-    res.redirect('/')
-})
 
 app.use(('/'), (req, res, next) => {
     console.log('In the next middlkeware')
