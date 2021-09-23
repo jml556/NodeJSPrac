3a16195e0350365cefd3e1de8546176d3d192672
Joe <jmleesjc2000@gmail.com>
2021-09-23 15:44:54 -0400

    update

--- first-app.js
+++ first-app.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const adminRoutes = require('./routes/admin.js')
+const shopRoutes = require('./routes/shop.js')
 
 const app = express()
 
 app.get('/favicon.ico', (req, res) => res.status(204));
 app.use(bodyParser.urlencoded({extended: true}))
 
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
 
-
-app.use(('/'), (req, res, next) => {
-    console.log('In the next middlkeware')
-    res.send('<h1>Hello Worldd</h1>')
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not Found</h1>')
 })
+
 
 app.listen(3000)
